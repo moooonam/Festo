8d5786407193521f5054ad1ab52c989a06d62810
Bellroute <kjkun7631@gmail.com>
2023-04-24 16:56:55 +0900

    init: 프로젝트 생성

---
+++ be/festo/src/main/java/com/example/festo/FestoApplication.java
@@ -0,0 +1,13 @@
+package com.example.festo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FestoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FestoApplication.class, args);
+    }
+
+}
