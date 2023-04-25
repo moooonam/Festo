9ecc9c1e9d35f0d96d10ed5c9d810a816be60f2e
SSAFY <anska77@naver.com>
2023-04-26 08:57:53 +0900

    feat : Add 네비게이션 바

--- fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeFragment.kt
+++ fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeFragment.kt
@@ -1,48 +1,48 @@
 package com.example.festo.customer_ui.home
 
-import android.app.AlertDialog
-import android.content.DialogInterface
+
+
 import android.os.Bundle
-import android.util.Log
+
 import android.view.LayoutInflater
+
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+
 import com.example.festo.R
 import com.example.festo.databinding.FragmentHomeBinding
 
+
 class HomeFragment : Fragment() {
 
-    private var mBinding : FragmentHomeBinding? = null
+    private var mBinding: FragmentHomeBinding? = null
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 
-       var binding = FragmentHomeBinding.inflate(inflater, container, false)
+        var binding = FragmentHomeBinding.inflate(inflater, container, false)
         mBinding = binding
-     /*   val notificotionBtn = binding.notificationBtn
-        notificotionBtn.setOnClickListener{
-            showDialog()
-        }*/
-        return  mBinding?.root
+        mBinding!!.testBtn.setOnClickListener {
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.layout_nav_bottom, FestivallistFragment())
+            transaction?.commit()
+//            return@setOnClickListener inflater.inflate(R.layout.fragment_festivallist, container, false)
+        }
+
+        mBinding!!.notificationBtn.setOnClickListener{
+            val transaction = fragmentManager?.beginTransaction()
+            transaction?.replace(R.id.layout_nav_bottom, NotificationFragment())
+            transaction?.commit()
+        }
+
+
+        return mBinding?.root
+
     }
-
- /*   private fun showDialog() {
-        val array = arrayOf("dog", "cat", "cow")
-        AlertDialog.Builder(this)
-            .setTitle("list")
-            .setItems(array, object : DialogInterface.OnClickListener {
-                override fun onClick(dialog: DialogInterface, which: Int) {
-                    val currentItem = array[which]
-                    Log.d("MyTag", "currentItem : $currentItem")
-                }
-            })
-            .show()
-
-    }*/
 
     override fun onDestroyView() {
         mBinding = null
