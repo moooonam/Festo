cd241c679e60b20eaa336ebd27a77bd8139a0df4
정채은 <jce0497@naver.com>
2023-04-26 09:04:29 +0900

    feat: Update 결제버튼 구현중

--- fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeActivity.kt
+++ fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeActivity.kt
@@ -1,13 +1,16 @@
 package com.example.festo.customer_ui.home
 
-import android.content.Intent
-import android.os.Bundle
-import android.widget.Button
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI
+import com.example.festo.R
 import com.example.festo.databinding.ActivityHomeBinding
-
+import com.example.festo.databinding.FragmentHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
@@ -19,24 +22,14 @@ class HomeActivity : AppCompatActivity() {
         mBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
         // 네비게이션들을 담는 호스트
-        val navHostFragment = supportFragmentManager.findFragmentById(com.example.festo.R.id.my_nav_host) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.my_nav_host) as NavHostFragment
         // 네이게이션 컨트롤러
         val navController = navHostFragment.navController
 
         //바텀 네비게이션 뷰와 네비게이션을 묶어준다
         NavigationUI.setupWithNavController(mBinding.myBottomNav, navController)
 
-
-        // 지워야함
-        val intent = Intent(this, FestivalActivity::class.java)
-
-        val bt1 = findViewById<Button>(com.example.festo.R.id.btn1)
-        bt1.setOnClickListener {
-            startActivity(intent)
-        }
     }
-
-
 
 
     /*  override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -44,5 +37,4 @@ class HomeActivity : AppCompatActivity() {
           return true
       }*/
 }
-
 
