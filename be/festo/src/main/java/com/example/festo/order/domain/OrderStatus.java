4e96b90fddedaba8d35dbcd3d07905469b949ac2
Bellroute <kjkun7631@gmail.com>
2023-04-28 17:35:24 +0900

    feat: order 상태 변경 api 1차 구현

--- be/festo/src/main/java/com/example/festo/order/domain/OrderStatus.java
+++ be/festo/src/main/java/com/example/festo/order/domain/OrderStatus.java
@@ -6,10 +6,10 @@ public enum OrderStatus {
     WAITING_ACCEPTANCE("주문 수락 대기"),
     COMPLETE_ACCEPTANCE("주문 수락 완료"),
     PREPARING_ORDER("상품 준비중"),
-
     READY_ORDER("상품 준비 완료"),
     WAITING_RECEIVE("수령 대기"),
-    COMPLETE("수령 완료");
+    RECEIVE_COMPLETE("수령 완료"),
+    COMPLETE("거래 완료");
 
     OrderStatus(String value) {
     }
