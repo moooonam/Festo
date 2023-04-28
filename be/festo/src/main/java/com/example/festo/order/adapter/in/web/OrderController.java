8982d2886e1ab9ccf1f3848cb7612c81205cd016
Bellroute <kjkun7631@gmail.com>
2023-04-28 15:33:03 +0900

    feat: 주문 등록 시 로그인 유저 정보 추가

--- be/festo/src/main/java/com/example/festo/order/adapter/in/web/OrderController.java
+++ be/festo/src/main/java/com/example/festo/order/adapter/in/web/OrderController.java
@@ -4,6 +4,8 @@ import com.example.festo.order.adapter.in.web.model.OrderRequest;
 import com.example.festo.order.application.port.in.PlaceOrderUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,8 +16,14 @@ public class OrderController {
 
     private final PlaceOrderUseCase placeOrderUseCase;
 
-    @PostMapping("/api/v1/order")
+    @PostMapping("/api/v1/orders")
     public ResponseEntity<Void> order(@RequestBody OrderRequest orderRequest) {
+        User user = (User) SecurityContextHolder.getContext()
+                                                .getAuthentication()
+                                                .getPrincipal();
+
+        orderRequest.setOrdererMemberId(Long.parseLong(user.getUsername()));
+
         placeOrderUseCase.placeOrder(orderRequest);
 
         return ResponseEntity.ok()
