362c4e31c4db28bb1754203a253655f4502173a3
장진문 <jjm1261@naver.com>
2023-05-01 09:52:22 +0900

    feat : search page/ main page

--- fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeActivity.kt
+++ fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeActivity.kt
@@ -1,23 +1,9 @@
 package com.example.festo.customer_ui.home
 
 
-import android.content.ClipData.Item
-import android.os.Build.VERSION_CODES.P
 import android.os.Bundle
-import android.util.Log
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
-import android.view.View
-import android.widget.Button
-import android.widget.Toast
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentTransaction
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.NavigationUI
 import com.example.festo.R
 import com.example.festo.customer_ui.mypage.MypageFragment
 import com.example.festo.customer_ui.orderlist.OrderlistFragment
@@ -25,53 +11,70 @@ import com.example.festo.customer_ui.recent.RecentFragment
 import com.example.festo.customer_ui.search.SearchFragment
 import com.example.festo.databinding.ActivityHomeBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.google.android.material.navigation.NavigationView
 
 
 class HomeActivity : AppCompatActivity() {
+
+    // 페스티벌 더미 리스트
+    var FestiList = mutableListOf<FestiList>(
+        FestiList(R.drawable.festival1, "진주 남강유등축제"),
+        FestiList(R.drawable.festival2, "광양 전통숯불구이축제"),
+        FestiList(R.drawable.festival1, "진주 남강유등축제"),
+        FestiList(R.drawable.festival2, "광양 전통숯불구이축제"),
+    )
 
     private lateinit var mBinding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         mBinding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_home)
-        supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, HomeFragment()).commit()
+
+        val Adapter = FestListAdapter(this, FestiList)
+        val list_view = findViewById<ListView>(R.id.festivalListView)
+        list_view.adapter = Adapter
+        // 고른 축제 페이지 연결
+
+        supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, HomeFragment())
+            .commit()
         // 네비게이션들을 담는 호스트
         val TAG: String = "HomeActivity : "
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.my_bottom_nav)
         bottomNavigationView.setOnItemSelectedListener {
-            when(it.itemId) {
+            when (it.itemId) {
                 R.id.homeFragment -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, HomeFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_nav_bottom, HomeFragment()).commit()
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.searchFragment -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, SearchFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_nav_bottom, SearchFragment()).commit()
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.recentFragment -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, RecentFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_nav_bottom, RecentFragment()).commit()
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.orderlistFragment -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, OrderlistFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_nav_bottom, OrderlistFragment()).commit()
                     return@setOnItemSelectedListener true
                 }
+
                 R.id.mypageFragment -> {
-                    supportFragmentManager.beginTransaction().replace(R.id.layout_nav_bottom, MypageFragment()).commit()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.layout_nav_bottom, MypageFragment()).commit()
                     return@setOnItemSelectedListener true
                 }
             }
             true
         }
-
-
     }
-
-
-
 
 
 }
--- fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeFragment.kt
+++ fe/festo/app/src/main/java/com/example/festo/customer_ui/home/HomeFragment.kt
@@ -23,7 +23,6 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         var binding = FragmentHomeBinding.inflate(inflater, container, false)
         mBinding = binding
         mBinding!!.testBtn.setOnClickListener {
@@ -38,7 +37,6 @@ class HomeFragment : Fragment() {
             transaction?.replace(R.id.layout_nav_bottom, NotificationFragment())
             transaction?.commit()
         }
-
 
         return mBinding?.root
 
