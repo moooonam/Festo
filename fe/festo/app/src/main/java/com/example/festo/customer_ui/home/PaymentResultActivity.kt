cea5dad24676e845724a7b19f0e9d9f68c11fa1e
정채은 <jce0497@naver.com>
2023-05-02 23:57:13 +0900

    feat: Add 결제위젯 연동, 축제에 등록된 부스 리스트 출력

---
+++ fe/festo/app/src/main/java/com/example/festo/customer_ui/home/PaymentResultActivity.kt
@@ -0,0 +1,46 @@
+package com.example.festo.customer_ui.home
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.festo.R
+
+
+class PaymentResultActivity : AppCompatActivity() {
+    companion object {
+        private const val EXTRA_RESULT = "extraResult"
+        private const val EXTRA_DATA = "extraData"
+
+        fun getIntent(context: Context, result: Boolean, data: ArrayList<String>): Intent {
+            return Intent(context, PaymentResultActivity::class.java).apply {
+                putExtra(EXTRA_RESULT, result)
+                putStringArrayListExtra(EXTRA_DATA, data)
+            }
+        }
+    }
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_result)
+
+        val isSuccess = intent?.getBooleanExtra(EXTRA_RESULT, false) == true
+        val resultDataList = intent?.getStringArrayListExtra(EXTRA_DATA).orEmpty()
+
+        val resultText = findViewById<TextView>(R.id.result)
+        resultText.text = isSuccess.toString()
+
+        // 주문내역으로 이동
+//        val goOrderListBtn = findViewById<Button>(R.id.goOrderList)
+
+//        goOrderListBtn.setOnClickListener{
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(R.id.orderlistFragment, OrderlistFragment())
+//                .commit()
+//        }
+    }
+}
