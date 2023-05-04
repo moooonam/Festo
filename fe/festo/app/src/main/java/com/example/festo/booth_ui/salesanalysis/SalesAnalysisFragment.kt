e8873fd4bb049022578988960f4ffa1b0bebe02c
정채은 <jce0497@naver.com>
2023-05-04 16:42:27 +0900

    feat: Add 매출 분석 페이지 인기메뉴 및 새로운 메뉴 추천 리사이클러뷰 생성

--- fe/festo/app/src/main/java/com/example/festo/booth_ui/salesanalysis/SalesAnalysisFragment.kt
+++ fe/festo/app/src/main/java/com/example/festo/booth_ui/salesanalysis/SalesAnalysisFragment.kt
@@ -5,10 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.festo.R
 import com.example.festo.databinding.FragmentBoothSalesanalysisBinding
+import com.example.festo.host_ui.boothlist.BoothListData
+import com.example.festo.host_ui.boothlist.BoothlistAdapter
 
+class NewMenuData(
+    var image: Int? = null,
+    var name: String? = null,
+)
 class SalesAnalysisFragment : Fragment() {
-
+    private lateinit var listAdapter: NewMenuListAdapter
     private var mBinding : FragmentBoothSalesanalysisBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -19,6 +28,18 @@ class SalesAnalysisFragment : Fragment() {
 
         mBinding = binding
         return  mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var NewMenuDataList : ArrayList <NewMenuData> = arrayListOf(
+            NewMenuData(R.drawable.logo1,"까사꼬치"),
+            NewMenuData(R.drawable.logo2,"까사꼬치"),
+            NewMenuData(R.drawable.logo3,"까사꼬치")
+        )
+        listAdapter = NewMenuListAdapter(NewMenuDataList)
+        mBinding?.newMenuListView?.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        mBinding?.newMenuListView?.adapter = listAdapter
     }
 
     override fun onDestroyView() {
