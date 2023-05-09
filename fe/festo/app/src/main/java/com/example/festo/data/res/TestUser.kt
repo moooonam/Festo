876b8341cb5be2cb386e7f217f698b5e5f8cfa69
정채은 <jce0497@naver.com>
2023-05-09 11:15:53 +0900

    feat: Update 오류 주석처리

--- fe/festo/app/src/main/java/com/example/festo/data/res/TestUser.kt
+++ fe/festo/app/src/main/java/com/example/festo/data/res/TestUser.kt
@@ -12,21 +12,21 @@ val website: String,
 val company: Company
 )
 
-data class Address(
-    val street: String,
-    val suite: String,
-    val city: String,
-    val zipcode: String,
-    val geo: Geo
-)
-
-data class Geo(
-    val lat: String,
-    val lng: String
-)
-
-data class Company(
-    val name: String,
-    val catchPhrase: String,
-    val bs: String
-)
+//data class Address(
+//    val street: String,
+//    val suite: String,
+//    val city: String,
+//    val zipcode: String,
+//    val geo: Geo
+//)
+//
+//data class Geo(
+//    val lat: String,
+//    val lng: String
+//)
+//
+//data class Company(
+//    val name: String,
+//    val catchPhrase: String,
+//    val bs: String
+//)
