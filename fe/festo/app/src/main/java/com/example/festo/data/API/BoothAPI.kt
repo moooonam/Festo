b9491f2f9c233bd4c0a2214c73e0fc5f12ebed1a
SSAFY <anska77@naver.com>
2023-05-10 15:40:47 +0900

    fix: conflict 수정

--- fe/festo/app/src/main/java/com/example/festo/data/API/BoothAPI.kt
+++ fe/festo/app/src/main/java/com/example/festo/data/API/BoothAPI.kt
@@ -1,25 +1,18 @@
 package com.example.festo.data.API
 
 import com.example.festo.data.req.RegiBoothRequest
-import com.example.festo.data.req.RegiFestivalRequest
-import com.example.festo.data.req.RegisterBoothReq
 import com.example.festo.data.res.BoothOrderListCompleteRes
 import com.example.festo.data.res.BoothOrderListRes
 import com.example.festo.data.res.FestivalIdRes
-import com.example.festo.data.res.RegisterBoothRes
-import com.example.festo.data.res.TestUser
 import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.Headers
-<<<<<<< fe/festo/app/src/main/java/com/example/festo/data/API/BoothAPI.kt
 import retrofit2.http.Multipart
 import retrofit2.http.POST
 import retrofit2.http.Part
-import retrofit2.http.POST
-import retrofit2.http.Query
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface BoothAPI {
 
@@ -27,17 +20,21 @@ interface BoothAPI {
     @Multipart
     @Headers("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgzNjkzNDUxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODg4Nzc0NTEsInN1YiI6IjEiLCJpc3MiOiJPdG16IiwiaWF0IjoxNjgzNjkzNDUxfQ.SYVxlhNtpJ7dJZILRo4IK-PKejaocbVciEk6Fo6raI4")
     @POST("festivals/{festival_id}/booths")
-    fun registerBooth(@Path("festival_id") festival_id:String, @Part ("request") request: RegiBoothRequest,  @Part boothImg: MultipartBody.Part): Call<Long>
+    fun registerBooth(
+        @Path("festival_id") festival_id: String,
+        @Part("request") request: RegiBoothRequest,
+        @Part boothImg: MultipartBody.Part
+    ): Call<Long>
 
     // 부스 신규,준비중 주문내역 불러오기
     @Headers("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgzNjkzNDUxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODg4Nzc0NTEsInN1YiI6IjEiLCJpc3MiOiJPdG16IiwiaWF0IjoxNjgzNjkzNDUxfQ.SYVxlhNtpJ7dJZILRo4IK-PKejaocbVciEk6Fo6raI4")
     @GET("booths/{booth_id}/orders?completed=false")
-    fun getBoothOrderList(@Path("booth_id") booth_id:String): Call<List<BoothOrderListRes>>
+    fun getBoothOrderList(@Path("booth_id") booth_id: String): Call<List<BoothOrderListRes>>
 
     // 부스 완료된 주문내역 불러오기
     @Headers("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgzNjkzNDUxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODg4Nzc0NTEsInN1YiI6IjEiLCJpc3MiOiJPdG16IiwiaWF0IjoxNjgzNjkzNDUxfQ.SYVxlhNtpJ7dJZILRo4IK-PKejaocbVciEk6Fo6raI4")
     @GET("booths/{booth_id}/orders?completed=true")
-    fun getBoothOrderListComplete(@Path("booth_id") booth_id:String): Call<List<BoothOrderListCompleteRes>>
+    fun getBoothOrderListComplete(@Path("booth_id") booth_id: String): Call<List<BoothOrderListCompleteRes>>
 
     // 부스 등록 전 축제 코드 입력
     @Headers("Authorization: Bearer eyJ0eXAiOiJKV1QiLCJyZWdEYXRlIjoxNjgzNjk0MjAxLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2ODg4NzgyMDEsInN1YiI6IjIiLCJpc3MiOiJPdG16IiwiaWF0IjoxNjgzNjk0MjAxfQ.AeidYEQ6RWsFYvzh6z1l990YGjAFHCkfiKV85UU2D7E")
