34d12f2714c8205cc4d6ba97cf371304a6bff465
Bellroute <kjkun7631@gmail.com>
2023-05-11 09:49:07 +0900

    feat: @Valid 추가

--- be/festo/src/main/java/com/example/festo/order/adapter/in/web/model/OrderMenu.java
+++ be/festo/src/main/java/com/example/festo/order/adapter/in/web/model/OrderMenu.java
@@ -1,5 +1,6 @@
 package com.example.festo.order.adapter.in.web.model;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class OrderMenu {
     @NotBlank(message = "[Request] menuId는 빈 값일 수 없습니다.")
     private Long menuId;
 
+    @Min(value = 1, message = "[Request] quantity는 최소 1 이상이어야 합니다.")
     @NotBlank(message = "[Request] quantity는 빈 값일 수 없습니다.")
     private Integer quantity;
 }
--- be/festo/src/main/java/com/example/festo/product/adapter/in/web/model/ProductRegisterRequest.java
+++ be/festo/src/main/java/com/example/festo/product/adapter/in/web/model/ProductRegisterRequest.java
@@ -1,5 +1,6 @@
 package com.example.festo.product.adapter.in.web.model;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ public class ProductRegisterRequest {
     @NotBlank(message = "[Request] productName는 빈 값일 수 없습니다.")
     private final String productName;
 
+    @Min(value = 0, message = "[Request] price는 최소 0 이상이어야합니다.")
     @NotBlank(message = "[Request] price는 빈 값일 수 없습니다.")
     private final int price;
 
