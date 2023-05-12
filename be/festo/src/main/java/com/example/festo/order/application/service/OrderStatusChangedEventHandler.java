e5a475de7009200c0fafc44322e5f522fd5e1b45
이충무 <lcm2822@daum.net>
2023-05-12 14:48:43 +0900

    feature: - FCM연동 및 메세지 보내는 로직 작성

--- be/festo/src/main/java/com/example/festo/order/application/service/OrderStatusChangedEventHandler.java
+++ be/festo/src/main/java/com/example/festo/order/application/service/OrderStatusChangedEventHandler.java
@@ -17,13 +17,22 @@ public class OrderStatusChangedEventHandler {
     public void handle(OrderStatusChangedEvent event) throws InterruptedException {
         Thread.sleep(2000);
 
-        // TODO fcm으로 메시지 보내기
-        // TODO 알림 메시지 db에 저장하기
+
+
+
+        //order에서 상태값이 변경되는 경우에 이벤트발생 - 완
+        //envent에 넘어와야할 데이터 -> OrderStatusChangedEvent
+        //getMemberTokenOutPort -> ownerId, ordererId
+
+        //status확인해 보내야할 사람이 2인경우는 FirebaseService.sendMessageTo(ownerId, 제목, 내용);
+
+        // TODO 알림 메시지 db에 저장하기 -> 아웃포트 알림목록 DB저장
 
         String status = event.getOrderStatus();
 
         if (status.equals("WAITING_ACCEPTANCE")) { // 부스 관리자에게 보낼 메시지
             log.info("ORDER STATUS CHANGED EVENT: 받는 사람 - {}, 상태 - {}", event.getBoothOwnerId(), event.getOrderStatus());
+            //로직 ownerId memberId
         } else if (status.equals("PREPARING_ORDER") || status.equals("WAITING_RECEIVE")) { // 주문자에게 보낼 메시지
             log.info("ORDER STATUS CHANGED EVENT: 받는 사람 - {}, 상태 - {}", event.getOrdererId(), event.getOrderStatus());
         } else { // 부스 관리자와 주문자 모두에게 보낼 메시지
